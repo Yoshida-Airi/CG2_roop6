b4972856d14a2d35f38397173547d7475e2837fa
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-05-10 02:59:17 +0900

    w

--- Windows.h
+++ Windows.h
@@ -6,19 +6,9 @@
 class Window
 {
 public:
-    Window(const wchar_t* title, int width, int height);
-    ~Window();
-
-    bool Create();
-    void Show(int nCmdShow);
-    void MessageLoop();
+	bool Initialize(LPCWSTR pTitle, int width, int height);
 
 private:
-    static LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
-    const wchar_t* m_title;
-    int m_width;
-    int m_height;
-    HWND m_hwnd;
+	LPCWSTR pTitle;
 };
 
