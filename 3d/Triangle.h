a6226c24854c7d96652c21a36f87c022cc92368b
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-06-29 21:56:36 +0900

    複数描画完成

--- 3d/Triangle.h
+++ 3d/Triangle.h
@@ -1,171 +1,34 @@
 #pragma once
-#include<Windows.h>
-#include<cstdint>
-#include <string>
-#include<format>
-#include<d3d12.h>
-#include<dxgi1_6.h>
-#include<cassert>
-#include<dxgidebug.h>
-#include<dxcapi.h>
-#include<Vector>
-
 #include"DirectX.h"
-#include"WindowAPI.h"
-#include "ConvertString.h"
+#include"Vector4.h"
 
-#pragma comment(lib,"d3d12.lib")
-#pragma comment(lib,"dxgi.lib")
-#pragma comment(lib,"dxguid.lib")
-#pragma comment(lib,"dxcompiler.lib")
-
-struct Vector4
-{
-	float x, y, z, w;
-};
-
-struct Vector3
-{
-	float x;
-	float y;
-	float z;
-};
-
+class MyEngine;
 
 class Triangle
 {
+
 public:
-	/// <summary>
-	/// コンストラクタ
-	/// </summary>
-	Triangle();
-	/// <summary>
-	/// デストラクタ
-	/// </summary>
-	~Triangle();
+	void Initialize(DirectX* direct);
 
-	/// <summary>
-	/// 初期化
-	/// </summary>
-	/// <param name="device"></param>
-	/// <param name="commandList"></param>
-	void Initialize();
+	void Draw(const Vector4& a, const Vector4& b, const Vector4& c);
 
-	/// <summary>
-	/// 更新処理
-	/// </summary>
-	/// <param name="direct"></param>
-	void Run();
-
-	/// <summary>
-	/// 終了(解放処理)
-	/// </summary>
 	void End();
 
 
+private:
+	MyEngine* engine_;
+
+	DirectX* direct_;
+
+	Vector4* vertexData_;
+
+	ID3D12Resource* vertexResource_;
+
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_;
 
 private:
-	DirectX* direct_ = new DirectX;
-	WindowAPI winApp_;
-
-	const int vertexCount = 3;	//三角形の頂点数
-	const int objectCount = 10;	//三角形の数
-	const int totalvartex = vertexCount * objectCount;	//合計の頂点数
-
-	HRESULT hr_;				//結果確認用
-	IDxcUtils* dxcUtils_ = nullptr;
-	IDxcCompiler3* dxcCompiler_ = nullptr;
-	IDxcIncludeHandler* includeHandler_ = nullptr;
-	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature_{};	//RootSignature作成
-	ID3D12RootSignature* rootSignature_ = nullptr;	//バイナリを元に生成
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs_[1] = {};//InputLayout
-	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc_{};
-	D3D12_BLEND_DESC blendDesc_{};//BlendStateの設定
-	D3D12_RASTERIZER_DESC rasterizerDesc_{};//RasiterzerStateの設定
-	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc_{};	//PSO
-	ID3D12PipelineState* graphicsPipelineState_ = nullptr;	//実際に生成
-	D3D12_RESOURCE_DESC vertexResourceDesc_{};	//頂点リソースの設定
-	ID3D12Resource* vertexResource_ = { nullptr };	//実際に頂点リソースを作る
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_{};//頂点バッファビューを作成する
-	D3D12_VIEWPORT viewport_{};	//ビューポート
-	D3D12_RECT scissorRect_{};//シザー矩形
-	UINT backBufferIndex;
+	void SetVertex();
 
 
-	ID3DBlob* signatureBlob_ = nullptr;//シリアライズしてバイナリにする
-	ID3DBlob* errorBlob_ = nullptr;
-
-	IDxcBlob* vertexShaderBlob_;
-	IDxcBlob* pixelShaderBlob_;
-
-	D3D12_CPU_DESCRIPTOR_HANDLE RTVHandle_[2];
-	uint64_t fenceValue_;
-
-	//コンパイルシェーダー関数
-	IDxcBlob* CompileShader
-	(//compilerするshaderファイルへのパス
-		const std::wstring& filePath,
-		//Compilerに使用するprofile
-		const wchar_t* profile,
-		//初期化で生成したものを3つ
-		IDxcUtils* dxcUtils,
-		IDxcCompiler3* dxcCompiler,
-		IDxcIncludeHandler* includeHandler
-	);
-
-	/// <summary>
-	/// DXCの初期化
-	/// </summary>
-	void IntializeDXC();
-
-	/// <summary>
-	/// RootSignatureの生成
-	/// </summary>
-	void CreateSignature();
-
-	/// <summary>
-	/// InputLayoutの生成
-	/// </summary>
-	void InputLayout();
-
-	/// <summary>
-	/// BlendState
-	/// </summary>
-	void BlendState();
-
-	/// <summary>
-	/// ラスタライザステートの設定
-	/// </summary>
-	void RasterizerState();
-
-	/// <summary>
-	/// シェーダーを取り込む
-	/// </summary>
-	void ShaderCompile();
-
-	/// <summary>
-	/// PSOの生成
-	/// </summary>
-	void CreatePSO();
-
-	/// <summary>
-	/// 頂点データ用のリソース
-	/// </summary>
-	void VertexResource();
-
-	/// <summary>
-	/// リソースに書き込む
-	/// </summary>
-	void Resource();
-
-	/// <summary>
-	/// 描画処理
-	/// </summary>
-	void Render();
-
-	/// <summary>
-	/// 解放処理
-	/// </summary>
-	void HandleClose();
 
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include<Windows.h>
 #include"WindowAPI.h"
 #include"DirectX.h"
+#include"Engine.h"
+
 #include"Triangle.h"
 
 //Windowsアプリでのエントリーポイント(main関数)
@@ -8,13 +10,50 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	WindowAPI* window = new WindowAPI;
 	DirectX* direct = new DirectX;
-	Triangle* triangle = new Triangle;
+	Engine* engine = new Engine;
+
 
 
 	//アプリケーションの開始
 	window->StartApp();
 	direct->Initialize(window->GetHwnd());
-	triangle->Initialize();
+	engine->Initialize();
+
+	Vector4 data1[3];
+	Vector4 data2[3];
+	Vector4 data3[3];
+
+	for (int i = 0; i < 3; i++)
+	{
+
+		data1[i] = { -0.2f,-0.2f + (i * -0.5f),0.0f,2.0f };
+		data2[i] = { 0.0f,0.2f + (i * -0.5f),0.0f,2.0f };
+		data3[i] = { 0.2f,-0.2f + (i * -0.5f),0.0f,2.0f };
+	}
+
+	Vector4 data4[3];
+	Vector4 data5[3];
+	Vector4 data6[3];
+
+	for (int i = 0; i < 3; i++)
+	{
+
+		data4[i] = { 0.2f,-0.2f + (i * -0.5f),0.0f,2.0f };
+		data5[i] = { 0.4f,0.2f + (i * -0.5f),0.0f,2.0f };
+		data6[i] = { 0.6f,-0.2f + (i * -0.5f),0.0f,2.0f };
+	}
+
+	Vector4 data7[4];
+	Vector4 data8[4];
+	Vector4 data9[4];
+
+	for (int i = 0; i < 4; i++)
+	{
+
+		data7[i] = { -0.6f,-0.2f + (i * -0.5f),0.0f,2.0f };
+		data8[i] = { -0.4f,0.2f + (i * -0.5f),0.0f,2.0f };
+		data9[i] = { -0.2f,-0.2f + (i * -0.5f),0.0f,2.0f };
+	}
 
 	/*=====================================*/
 	/* 　　　　   メインループ　　    　       */
@@ -31,17 +70,37 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		}
 		else
 		{
-			//ゲームの処理
-			triangle->Run();
+			engine->Run();
+
+			for (int i = 0; i < 3; i++)
+			{
+				//三角形描画
+				engine->DrawTriangle(data1[i], data2[i], data3[i]);
+			}
+
+			for (int i = 0; i < 3; i++)
+			{
+				//三角形描画
+				engine->DrawTriangle(data4[i], data5[i], data6[i]);
+			}
+
+			for (int i = 0; i < 4; i++)
+			{
+				//三角形描画
+				engine->DrawTriangle(data7[i], data8[i], data9[i]);
+			}
+
+			engine->RunEnd();
+
 		}
 	}
 
-	triangle->End();
+	window->EndApp();
+	engine->End();
 	direct->End();
 
 
-	//アプリケーションの終了
-	window->EndApp();
+
 
 	return 0;
 
